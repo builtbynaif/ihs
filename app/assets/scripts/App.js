9564029c45994691cf59044ea088bd35e76ff621
Naif <hello@builtbynaif.com>
2019-05-28 03:24:53 +0530

    all done

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,18 @@
+import $ from 'jquery';
+import slick from 'slick-carousel';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal';
+import HeroSlider from './modules/HeroSlider';
+import Responsive from './modules/Responsive';
+import SlideOnScroll from './modules/SlideOnScroll';
 
 var mobileMenu = new MobileMenu();
-new RevealOnScroll($(".feature-item"), "85%");
-new RevealOnScroll($(".testimonial"), "60%");
 var stickyheader = new StickyHeader();
 var modal = new Modal();
+var heroSlider = new HeroSlider(); 
+var responsive = new Responsive(); 
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%"); 
+new SlideOnScroll($(".slideIn"), "75%");
--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,17 +1,12 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
-import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
     constructor() {
         this.lazyImages = $(".lazyload");
         this.siteHeader = $(".site-header");
-        this.headerTriggerElement = $(".large-hero__title");
+        this.headerTriggerElement = $(".fullWidthText");
         this.createHeaderWaypoint();
-        this.pageSections = $(".page-section");
-        this.headerLinks = $(".primary-nav a");
-        this.createPageSectionWaypoints();
-        this.addSmoothScrolling();
         this.refreshWaypoints();
     }
 
@@ -20,12 +15,6 @@ class StickyHeader {
             Waypoint.refreshAll();
         });
     }
-
-
-    addSmoothScrolling() {
-        this.headerLinks.smoothScroll();
-    }
-
     createHeaderWaypoint() {
         var that = this;
         new Waypoint({
@@ -37,36 +26,6 @@ class StickyHeader {
                     that.siteHeader.removeClass("site-header--dark");
                 }
             }
-        });
-    }
-
-    createPageSectionWaypoints() {
-        var that = this;
-        this.pageSections.each(function () {
-            var currentPageSection = this;
-            new Waypoint({
-                element: currentPageSection,
-                handler: function (direction) {
-                    if (direction == "down") {
-                        var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
-                        that.headerLinks.removeClass("is-current-link");
-                        $(matchingHeaderLink).addClass("is-current-link");
-                    }
-                },
-                offset: "18%"
-            });
-
-            new Waypoint({
-                element: currentPageSection,
-                handler: function (direction) {
-                    if (direction == "up") {
-                        var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
-                        that.headerLinks.removeClass("is-current-link");
-                        $(matchingHeaderLink).addClass("is-current-link");
-                    }
-                },
-                offset: "-40%"
-            });
         });
     }
 }
